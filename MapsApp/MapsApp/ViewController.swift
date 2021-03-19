2099e85b932a1d2ceed2651723f0b36ec800e3fe
KasRoid <KasRoid@gmail.com>
2021-03-19 10:59:39 +0900

    feat: Setting Location Accuracy and Filter with CLLocationManager & Permissions

--- MapsApp/MapsApp/ViewController.swift
+++ MapsApp/MapsApp/ViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
     @IBOutlet weak var mapView: MKMapView!
+    fileprivate let locationManager = CLLocationManager()
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupMapView()
+        setupLocationManager()
     }
+}
+
+// MARK: - Map Related
+extension ViewController {
+    private func setupMapView() {
+        mapView.showsUserLocation = true
+        mapView.delegate = self
+    }
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+    
+}
+
+// MARK: - MKMapViewDelegate
+extension ViewController: MKMapViewDelegate {
+    
 }
