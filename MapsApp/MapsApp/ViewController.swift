cb7d4ebed7d94b40357c0df71b628a0584bc54e6
KasRoid <KasRoid@gmail.com>
2021-03-19 10:12:03 +0900

    init: Created Project and Setup Basics

---
+++ MapsApp/MapsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapsApp
+//
+//  Created by Kas Song on 2021/03/19.
+//
+
+import MapKit
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
